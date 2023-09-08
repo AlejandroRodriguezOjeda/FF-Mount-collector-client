8b94863e19f4235a4e47b1b6a6abac9d10b2e130
Alejandro Rodriguez <aperturegir@gmail.com>
2023-09-08 07:52:23 +0100

    trying to fix netlify

--- src/pages/MountDetails.jsx
+++ src/pages/MountDetails.jsx
@@ -1,26 +1,23 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-
 import { useNavigate } from "react-router";
-import { createCommentService, getCommentService } from '../services/service.comments'
+import {
+  createCommentService,
+  getCommentService,
+} from "../services/service.comments";
 import MountComment from "../components/mountComment.jsx";
-
 
 function MountDetails() {
   const [mountDetails, setMountDetails] = useState(null);
   const { id } = useParams();
-  const [addToFavs, setAddToFavs] = useState(true);
-  const [newComment, setNewComment] = useState("")
-  const [mountComments, setMountComments] = useState(null)
+  const [newComment, setNewComment] = useState("");
+  const [mountComments, setMountComments] = useState(null);
 
-  const handleCommentChange = (event) => setNewComment(event.target.value)
-//   const mountId = useParams()
+  const handleCommentChange = (event) => setNewComment(event.target.value);
+  //   const mountId = useParams()
 
-const navigate = useNavigate()
-
-
-
+  const navigate = useNavigate();
 
   useEffect(() => {
     getMountDetails();
@@ -33,10 +30,9 @@ const navigate = useNavigate()
       );
       console.log(response);
       setMountDetails(response.data);
-      
 
-      const response2 = await getCommentService(id)
-      setMountComments(response2.data)
+      const response2 = await getCommentService(id);
+      setMountComments(response2.data);
     } catch (error) {
       console.log("I have found an error", error);
     }
@@ -51,7 +47,6 @@ const navigate = useNavigate()
     }
   };
 
-
   const addComment = async (event) => {
     event.preventDefault();
     const comment = {
@@ -65,81 +60,68 @@ const navigate = useNavigate()
     }
   };
 
-//   const handleAddToOwned = async () => {
-//     try {
-//         const response = await service.post("/mounts/mark-as-owned", { mountId: id})
-//           console.log(response);
-
-//       navigate("/my-profile")
-//     } catch (error) {
-//       console.log("Error adding to owned:", error);
-//     }
-//   };
-
-
-
   if (mountDetails === null) {
     return <h3>...loading</h3>;
   }
 
-
-return (
-  <div>
+  return (
     <div>
-      <h4>{mountDetails.name}</h4>
-      <img src={mountDetails.image} alt="" />
-      <hr />
-      <b>{mountDetails.description}</b>
-      <hr />
-      <span>{mountDetails.enhanced_description}</span>
+      <div>
+        <h4>{mountDetails.name}</h4>
+        <img src={mountDetails.image} alt="" />
+        <hr />
+        <b>{mountDetails.description}</b>
+        <hr />
+        <span>{mountDetails.enhanced_description}</span>
 
-      <br />
+        <br />
 
-      <b>
-        seats: {mountDetails.seats} Patch: {mountDetails.patch}
-      </b>
+        <b>
+          seats: {mountDetails.seats} Patch: {mountDetails.patch}
+        </b>
 
-      <hr />
+        <hr />
 
-      <p>check out how it sounds:</p>
+        <p>check out how it sounds:</p>
 
-      <audio controls src={mountDetails.bgm} />
+        <audio controls src={mountDetails.bgm} />
 
-      {/* Hacer un link con el id  */}
-    </div>
+        {/* Hacer un link con el id  */}
+      </div>
 
-    <div>
-      {/* <button onClick={handleAddToOwned}>Add to owned:</button> */}
-      <Link to={`/new-favorite/${id}`}>
-      <button>Create favorite</button>
-      </Link>
-    </div>
+      <div>
+        {/* <button onClick={handleAddToOwned}>Add to owned:</button> */}
+        <Link to={`/new-favorite/${id}`}>
+          <button>Create favorite</button>
+        </Link>
+      </div>
 
-    <div>
-        <hr className='mount-comment' />
+      <div>
+        <hr className="mount-comment" />
 
-        
-  <h3>Deja tu comentario</h3>
-  <div className="comment-form">
-    <textarea
-      placeholder="Enter your comment here"
-      value={newComment}
-      onChange={handleCommentChange}
-    />
-    <button onClick={addComment}>Comentar</button>
-  </div>
+        <h3>Deja tu comentario</h3>
+        <div className="comment-form">
+          <textarea
+            placeholder="Enter your comment here"
+            value={newComment}
+            onChange={handleCommentChange}
+          />
+          <button onClick={addComment}>Comentar</button>
+        </div>
 
         <h3>Comments about this mount</h3>
         {mountComments !== null ? (
-   <MountComment mountComments={mountComments} updateComments={getMountDetails} />
- 
-  ) : (
-    <p>No comments available.</p>
-  )}
+          <MountComment
+            mountComments={mountComments}
+            updateComments={getMountDetails}
+          />
+        ) : (
+          <p>No comments available.</p>
+        )}
       </div>
 
-    {/* cambiar base de datos para deje de salir el error hex */}
-  </div>
-);
+      {/* cambiar base de datos para deje de salir el error hex */}
+    </div>
+  );
 }
 export default MountDetails;
